8ca26e548abaa608391a94670b7d414d12b9ab98
ArtemEfimovich <artemfender@gmail.com>
2023-01-22 11:15:14 +0300

    sort table

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,18 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            if (action.payload === 'up') {
+                 state.sort((u, u2) => u.name.localeCompare(u2.name))
+            } else {
+               state.sort((u, u2) => u2.name.localeCompare(u.name))
+            }
+            return state
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter((u)=> u.age >= action.payload)
         }
         default:
             return state
